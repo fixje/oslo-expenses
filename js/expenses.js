5e039b8b47fa3adb8d8cefd15835d5357af6664c
Markus Fuchs <mfuchs13@gmail.com>
2013-05-12 23:07:39 +0200

    added templating, jquery and tabs

--- js/expenses.js
+++ js/expenses.js
@@ -235,90 +235,29 @@ function OsloExpenses(datasource, options) {
     }
     this.anchor.appendChild(this.loader);
 
-    //set up nodes we need
+    this.templates = {};
     this.nodes = {};
-    this.nodes.container = {
-        monthly: document.createElement("div"),
-        weekly: document.createElement("div"),
-        index: document.createElement("div"),
-        weekly_trend: document.createElement("div"),
-        monthly_index: document.createElement("div"),
-        weekly_index: document.createElement("div")
-    };
-    this.nodes.container.monthly.setAttribute("id", "monthly_container");
-    this.nodes.container.weekly.setAttribute("id", "weekly_container");
-    this.nodes.container.monthly.style.visibility = "hidden";
-    this.nodes.container.weekly.style.visibility = "hidden";
-    this.nodes.container.index.style.visibility = "hidden";
-    this.nodes.container.weekly_trend.style.visibility = "hidden";
 
-    this.nodes.content = {
-        monthly: document.createElement("div"),
-        weekly: document.createElement("div"),
-        weekly_trend: document.createElement("div")
+    // load template
+    $.get("main.tmpl.html", function(data) {
+        $(self.anchor).append(data);
+        self.templates.stats = $("#item-stat-template").val();
+        self.templates.navItem = "<a href=\"{reference}\" data-toggle=\"tab\">{title}</a>";
+        self.nodes.navCurrentWeek = $("#nav-current-week")[0];
+        self.nodes.navCurrentMonth = $("#nav-current-month")[0];
+        self.nodes.navMonths = $("#nav-months")[0];
+        self.nodes.navWeeks = $("#nav-weeks")[0];
+        self.nodes.tabContent = $("#tab-content")[0];
+        self.nodes.trendContainer = $("#trend-container")[0];
+        self.nodes.mainContainer = $("#expenses-container")[0];
 
-    };
-    this.nodes.content.monthly.setAttribute("id", "monthly_content");
-    this.nodes.content.weekly.setAttribute("id", "weekly_content");
-
-    this.nodes.index = {
-        monthly: document.createElement("ul"),
-        weekly: document.createElement("ul")
-    };
-    this.nodes.container.monthly_index.appendChild(this.nodes.index.monthly);
-    this.nodes.container.weekly_index.appendChild(this.nodes.index.weekly);
-
-    var tp = document.createElement("h4");
-    tp.textContent = "Index";
-    this.nodes.container.index.className = "well";
-    this.nodes.container.index.appendChild(tp);
-    this.nodes.container.index.appendChild(this.nodes.container.monthly_index);
-    this.nodes.container.index.appendChild(this.nodes.container.weekly_index);
-    this.anchor.appendChild(this.nodes.container.index);
-
-    tp = document.createElement("h2");
-    tp.innerHTML = "<a name=\"oETrend\">" + this.stringStore.S("trend") + "</a>";
-    this.nodes.container.weekly_trend.appendChild(tp);
-    tp = document.createElement("li");
-    tp.innerHTML = "<a href=\"#oETrend\">" + this.stringStore.S("trend") + "</a>";
-    this.nodes.index.weekly.appendChild(tp);
-    this.nodes.container.weekly_trend.appendChild(this.nodes.content.weekly_trend);
-    this.nodes.content.weekly_trend.className = "well";
-    this.anchor.appendChild(this.nodes.container.weekly_trend);
-
-    var pt = document.createElement("h2");
-    pt.textContent = this.stringStore.S("monthly_stats");
-    this.nodes.container.monthly.appendChild(pt);
-    this.nodes.container.monthly.appendChild(this.nodes.content.monthly);
-    this.anchor.appendChild(this.nodes.container.monthly);
-
-    var pt = document.createElement("h2");
-    pt.textContent = this.stringStore.S("weekly_stats");
-    this.nodes.container.weekly.appendChild(pt);
-    this.nodes.container.weekly.appendChild(this.nodes.content.weekly);
-    this.anchor.appendChild(this.nodes.container.weekly);
-
-    // set up preferences dropdown menu
-    var drop_container = document.createElement("div");
-    drop_container.className = "dropdown";
-    var drop_toggle = document.createElement("a");
-    drop_toggle.setAttribute("role", "button");
-    drop_toggle.setAttribute("data-toggle","dropdown");
-    drop_toggle.setAttribute("href", "#");
-    drop_toggle.innerHTML = "Properties";
-    var drop_menu = document.createElement("ul");
-    drop_menu.className = "dropdown-menu";
-    drop_menu.setAttribute("role", "menu");
-    drop_container.appendChild(drop_toggle);
-    drop_container.appendChild(drop_menu);
-    //this.nodes.container.index.appendChild(drop_container);
-
-    google.setOnLoadCallback(function() {
-        var query = new google.visualization.Query(datasource) ;
-        query.send(function(response) {
-            // this wrapper is necessary not to confuse the
-            // OsloExpense object!
-            self.processData(response) 
+        google.setOnLoadCallback(function() {
+            var query = new google.visualization.Query(datasource) ;
+            query.send(function(response) {
+                // this wrapper is necessary not to confuse the
+                // OsloExpense object!
+                self.processData(response) 
+            });
         });
     });
 }
@@ -346,87 +285,32 @@ OsloExpenses.prototype.stats2data = function(s) {
 /*
  * creates all visual elements
  */ 
-OsloExpenses.prototype.showStatData = function(anchor, stat_data, id_template, caption, hide_table_default, index_list) {
-    if(hide_table_default === undefined) { hide_table_default = true; }
+OsloExpenses.prototype.showStatData = function(anchor, stat_data, id_template, caption) {
     var ch_data = this.stats2data(stat_data);
-    /* Layout:
-     *
-     * ------- anchor div --------------
-     * |                               |
-     * | --------- n_div ------------- |
-     * | |                           | |
-     * | | <h2> Caption </h2>        | |
-     * | | <p> Total </p>            | |
-     * | | ----- chart_container --- | |
-     * | | |                       | | |
-     * | | ------------------------- | |
-     * | | <button> show list </btn> | |
-     * | | ----- list_container ---- | |
-     * | | |                       | | |
-     * | | ------------------------- | |
-     * | ----------------------------- |
-     * ---------------------------------
-     */
 
-    // prepare containers and control elements
-    var n_div = document.createElement("div");
-    n_div.setAttribute("id", "container-"+id_template);
-    n_div.className = "stat-container";
-    n_div.className = "well";
-    var chart_container = document.createElement("div");
-    chart_container.setAttribute("id", "chart-cont-"+id_template);
-    var list_container = document.createElement("div");
-    list_container.setAttribute("id", "list-"+id_template);
+    var template = $("#item-stat-template").html();
+    $(this.nodes.tabContent).append($.fn.nano(template, { reference: id_template, title: caption}));
+    var list_container = $("#stat-list-container-" + id_template)[0];
+    var chart_container = $("#stat-chart-container-" + id_template)[0];
+    var stats_container = $("#stat-stats-" + id_template)[0];
 
-    var caption_elem = document.createElement("h2");
-    var caption_anchor = document.createElement("a");
-    caption_anchor.setAttribute("name", id_template);
-    caption_elem.appendChild(caption_anchor);
-    caption_anchor.textContent = caption;
-
-    var index_elem = document.createElement("li");
-    var index_link = document.createElement("a");
-    index_link.setAttribute("href", "#"+id_template);
-    index_link.textContent = caption;
-    index_elem.appendChild(index_link);
-    index_list.appendChild(index_elem);
-
-    var p_total = document.createElement("p");
-    p_total.textContent =  this.stringStore.S("total_expenses") + ": " + stat_data.total.toFixed(2) + "€";
-
-    var btn = document.createElement("button");
-    if(hide_table_default) {
-        btn.textContent =  this.stringStore.S("show_list");
-        list_container.style.visibility = "hidden";
+    // add nav entry
+    var nav_template = "<li><a href=\"#{reference}\" data-toggle=\"tab\">{title}</a></li>";
+    $(anchor).append($.fn.nano(nav_template, {reference: id_template, title: caption}));
+    if ( anchor == this.nodes.navWeeks ) {
+        $(this.nodes.navCurrentWeek).attr("href", "#" + id_template);
     } else {
-        btn.textContent =  this.stringStore.S("hide_list");
+        $(this.nodes.navCurrentMonth).attr("href", "#" + id_template);
     }
-    var self = this;
-    btn.onclick = function() {
-        var list = document.getElementById("list-"+id_template);
-        if(list.style.visibility === "hidden") {
-            list.style.visibility = "visible";
-            list.style.height = "auto";
-            this.textContent =  self.stringStore.S("hide_list");
-        } else {
-            list.style.visibility = "hidden";
-            list.style.height = 0;
-            this.textContent =  self.stringStore.S("show_list");
-        }
-    }
+    // hack to make it visible
+    $("#nav-tabs-list a:last").tab("show");
+    $("#nav-tabs-list a:first").tab("show");
 
-
-    n_div.appendChild(caption_elem);
-    n_div.appendChild(p_total);
-    n_div.appendChild(chart_container);
-    n_div.appendChild(btn);
-    n_div.appendChild(list_container);
-    anchor.appendChild(n_div);
-    //
+    var stats_template = "<p>{title}: {value}€</p>";
+    $(stats_container).append($.fn.nano(stats_template, {title: this.stringStore.S("total_expenses"), value: stat_data.total.toFixed(2)}));
 
     var table_viz = new google.visualization.Table(list_container);
     table_viz.draw(google.visualization.arrayToDataTable(stat_data.coldata), null);
-    if(hide_table_default) { list_container.style.height = 0; };
 
     var options = {'title': caption,
         'width':500,
@@ -448,33 +332,24 @@ OsloExpenses.prototype.processData = function(response) {
         var cached_data = JSON.parse(localStorage.getItem("spreadsheet_data"));
 
         if(cached_data == null) {
-            var errDiv = document.createElement("div");
-            errDiv.className = "alert alert-error";
-            var ht = document.createElement("h4");
-            ht.textContent = "Error: " + response.getMessage();
-            var pt = document.createElement("p");
-            pt.innerHTML = response.getDetailedMessage();
-            errDiv.appendChild(ht);
-            errDiv.appendChild(pt);
-            this.anchor.appendChild(errDiv);
+            errorTemplate = $("#error-template").html();
+            $(this.anchor).append($.nano(errorTemplate,
+                        { title: response.getMessage(), 
+                          message: response.getDetailedMessage()
+                        }));
             this.hideLoader();
             return;
         } else {
-            var errDiv = document.createElement("div");
-            errDiv.className = "alert";
-            var pt = document.createElement("p");
-            pt.innerHTML = "There was an error retrieving the data. Showing cached copy";
-            errDiv.appendChild(pt);
-            this.anchor.appendChild(errDiv);
+            errorTemplate = $("#warning-template").html();
+            $(this.anchor).append($.nano(errorTemplate,
+                        { title: "Warning",
+                          message:  "There was an error retrieving the data. Showing cached copy"
+                        }));
             this.hideLoader();
             response.getDataTable = function() { return new google.visualization.DataTable(cached_data); };
         }
     }
     var data = response.getDataTable();
-
-    // draw table for all rows
-    //var table_viz = new google.visualization.Table(document.getElementById("table_container"));
-    //table_viz.draw(data, null);
 
     var monthly_stats = { };
     var weekly_stats = {  };
@@ -518,14 +393,14 @@ OsloExpenses.prototype.processData = function(response) {
     // produce monthly stats
     for (month in monthly_stats) {
         var s = monthly_stats[month];
-        self.showStatData(this.nodes.content.monthly, s, "stat-monthly"+month, mm[month], true, this.nodes.index.monthly);
+        self.showStatData(this.nodes.navMonths, s, "stat-monthly"+month, mm[month]);
     }
     //
 
     // produce weekly stats
     for (week in weekly_stats) {
         var s = weekly_stats[week];
-        self.showStatData(this.nodes.content.weekly, s, "stat-weekly"+week, "KW " + week, false, this.nodes.index.weekly);
+        self.showStatData(this.nodes.navWeeks, s, "stat-weekly"+week, "KW " + week);
     }
     //
 
@@ -535,24 +410,25 @@ OsloExpenses.prototype.processData = function(response) {
         var s = weekly_stats[week];
         wt.push([week, s.total ]); 
     }
-    this.makeTrendChart(google.visualization.arrayToDataTable(wt));
+    this.trendData = google.visualization.arrayToDataTable(wt);
+    this.drawTrendChart();
     //
 
     // hide loader and show content
-    this.nodes.container.monthly.style.visibility = "visible";
-    this.nodes.container.weekly.style.visibility = "visible";
-    this.nodes.container.index.style.visibility = "visible";
-    this.nodes.container.weekly_trend.style.visibility = "visible";
+    this.nodes.mainContainer.style.display = "block";
     this.hideLoader();
 
     // store data in cache
     localStorage.setItem("spreadsheet_data", data.toJSON());
 }
 
-OsloExpenses.prototype.makeTrendChart = function(data) {
-    var c = new google.visualization.LineChart(this.nodes.content.weekly_trend)
-        c.draw(data, {curveType: "function", width: 700, height: 300,
-            hAxis: {title: "KW"}, legend: {position: "none"}});
+OsloExpenses.prototype.drawTrendChart = function() {
+    if (this.trendChart == undefined) {
+        this.trendChart = new google.visualization.LineChart(this.nodes.trendContainer)
+    }
+    var self = this;
+    this.trendChart.draw(self.trendData, {curveType: "function", width: 700, height: 300,
+        hAxis: {title: "KW"}, legend: {position: "none"}});
 }
 
 OsloExpenses.prototype.hideLoader = function() {
